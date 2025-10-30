eb0e4cf1daeabfc29e1cd80d6cdf69a7db48483b
nalydrey <nalydrey@gmail.com>
2025-10-30 02:11:15 +0200

    cplusplus posibility in functions

--- functions/functions.h
+++ functions/functions.h
@@ -10,7 +10,16 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
 int16_t mapRange(uint16_t x, uint16_t in_min, uint16_t in_max, uint16_t out_min, uint16_t out_max);
+
+#ifdef __cplusplus
+}
+#endif
 
 
 #endif /* FUNCTIONS_FUNCTIONS_H_ */
